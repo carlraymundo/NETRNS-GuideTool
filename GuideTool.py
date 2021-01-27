60dff1b15584fc22b2690b9a11864da9e4b49fb0
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2021-01-27 21:31:07 +0800

    Create GuideTool.py

---
+++ GuideTool.py
@@ -0,0 +1,12 @@
+def main():
+  print("Hello there Network Admin!\n")
+  print("In order to help you, please select any of the following options:")
+  print("[1] Subnet Calculator")
+  print("[2] Check Address Class")
+  print("[3] Check Address Type")
+  print("[4] Exit\n")
+
+  menu = input("Input: ")
+  print(menu)
+
+main()
